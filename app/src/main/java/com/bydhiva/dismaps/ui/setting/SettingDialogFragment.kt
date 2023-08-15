91caeefc3b78ef96e6b43c4f0e23f5e2e72238ca
dhivatiradika <dhivatiradika@gmail.com>
2023-08-15 12:40:28 +0800

    up version and fix alert

--- app/src/main/java/com/bydhiva/dismaps/ui/setting/SettingDialogFragment.kt
+++ app/src/main/java/com/bydhiva/dismaps/ui/setting/SettingDialogFragment.kt
@@ -77,6 +77,9 @@ class SettingDialogFragment : DialogFragment() {
                  viewModel.saveAlertSetting(true)
                  viewModel.setWaterLevelWorker()
              }
+         } else {
+             viewModel.saveAlertSetting(true)
+             viewModel.setWaterLevelWorker()
          }
     }
 
